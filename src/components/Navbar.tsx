f5989d7cee2c70873511971a0989bbee1d5ff7d0
Niklas Tallund <niklastallund@gmail.com>
2025-10-07 15:29:12 +0200

    quick navbar skeleton

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="sticky top-0 z-50 bg-background/95 border-b border-border backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <h1>test</h1>
+    </nav>
+  );
+}
