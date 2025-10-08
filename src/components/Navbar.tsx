f8c30766ff44b6f3f7ddb59e1d734d73e00b864a
Niklas Tallund <niklastallund@gmail.com>
2025-10-08 11:47:59 +0200

    skeleton done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+import NavbarMenu from "./NavbarMenu";
+
 export default function Navbar() {
   return (
     <nav className="sticky top-0 z-50 bg-background/95 border-b border-border backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <h1>test</h1>
+      <div className="flex items-center justify-center p-2">
+        <Image
+          src="/tmplogo.png"
+          alt="Logo"
+          width={100}
+          height={100}
+          className="w-auto mx-0 px-5"
+        />
+        <NavbarMenu />
+      </div>
     </nav>
   );
 }
