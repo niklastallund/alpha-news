d5e1074a57041f03b972b59954f4cd268d362d3c
Tobias Eriksson <hungatobbe@gmail.com>
2025-10-28 14:47:22 +0100

    designstuff

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -11,10 +11,27 @@ import PasswordForm from "./PasswordForm";
 import NameForm from "./NameForm";
 import { prisma } from "@/lib/prisma";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
 export default async function UserPage() {
   const session = await getSessionData();
 
   if (!session) redirect("/");
+
+  const sub = await prisma.subscription.findFirst({
+    where: {
+      referenceId: session.user.id,
+    },
+  });
 
   const getUserData = async () => {
     const articles = await prisma.article.findMany({
@@ -58,25 +75,90 @@ export default async function UserPage() {
         </div>
 
         <div className="p-2 md:col-span-2 lg:col-span-3">
-          Data like subscription, articles, discussions(?)
           <br />
-          <span className="font-bold">Articles:</span>
-          <ul>
-            {userData.articles.map((a) => (
-              <li key={a.id}>{a.headline}</li>
-            ))}
-          </ul>
+
+          <Card>
+            <CardHeader>
+              <CardTitle>Subscription</CardTitle>
+              <CardDescription></CardDescription>
+            </CardHeader>
+            <CardContent className="flex-1">
+              Active: {JSON.stringify(sub)}.
+              <br />
+              Upgrade:
+              <br />
+              <div className="grid grid-cols-2">
+                <Card>
+                  <CardHeader>
+                    <CardTitle>Basic</CardTitle>
+                    <CardDescription></CardDescription>
+                  </CardHeader>
+                  <CardContent>
+                    69:- / Month
+                    <Button>Upgrade</Button>
+                  </CardContent>
+                </Card>
+
+                <Card>
+                  <CardHeader>
+                    <CardTitle>PRO</CardTitle>
+                    <CardDescription></CardDescription>
+                  </CardHeader>
+                  <CardContent>
+                    99:- / Month
+                    <Button>Upgrade</Button>
+                  </CardContent>
+                </Card>
+              </div>
+            </CardContent>
+          </Card>
+
+          <Card>
+            <CardHeader>
+              <CardTitle>Your articles:</CardTitle>
+              <CardDescription></CardDescription>
+            </CardHeader>
+            <CardContent className="flex-1">
+              <ul>
+                {userData.articles.map((a) => (
+                  <li key={a.id}>
+                    <Link href={"/article/" + a.id}>{a.headline}</Link>
+                  </li>
+                ))}
+              </ul>
+              <br />
+            </CardContent>
+          </Card>
+
           <br />
-          <br />
-          <span className="font-bold">Comments:</span>
-          <ul>
-            {userData.comments.map((a) => (
-              <li key={a.id}>
-                {a.article.headline?.slice(0, 33)} - {a.content.slice(0, 33)}...
-              </li>
-            ))}
-          </ul>
-          <br />
+
+          <Card>
+            <CardHeader>
+              <CardTitle>Your comments:</CardTitle>
+              <CardDescription></CardDescription>
+            </CardHeader>
+            <CardContent className="flex-1">
+              <ul>
+                {userData.comments.map((a) => (
+                  <li key={a.id}>
+                    <Link href={"/article/" + a.article.id}>
+                      <span className="font-bold">
+                        {a.article?.headline
+                          ? a.article.headline.slice(0, 15)
+                          : ""}
+                        {a.article?.headline &&
+                          a.article.headline.length > 15 &&
+                          "..."}{" "}
+                        -{" "}
+                      </span>
+                      {a.content.slice(0, 33)} {a.content.length > 33 && "..."}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+              <br />
+            </CardContent>
+          </Card>
         </div>
       </div>
     </Page>
