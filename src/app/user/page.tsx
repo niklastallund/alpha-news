111c805c44e7ea34f63138f1c732f5ebd236ea2b
Niklas Tallund <niklastallund@gmail.com>
2025-11-05 14:43:43 +0100

    asdasdasda

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,7 +1,6 @@
 // So this is the landingpage for the user after signing in, or clicked on user in nav.
 // Here the user can see its role, change profilepic, details and password.
 
-
 import Page from "@/components/Page";
 import { getSessionData } from "@/lib/actions/sessiondata";
 import { redirect } from "next/navigation";
@@ -53,7 +52,7 @@ export default async function UserPage() {
     <Page>
       <div className="w-full md:grid md:grid-cols-2 lg:grid-cols-3 gap-2">
         <div className="p-2 md:col-span-2 lg:col-span-3 md:flex md:flex-wrap md:gap-2 md:items-end">
-          {/* <UserPic></UserPic> */}
+          <UserPic></UserPic>
 
           <div>
             <span className="text-4xl">{session.user.name}</span>
@@ -65,7 +64,9 @@ export default async function UserPage() {
           </div>
         </div>
 
-        <div className="p-2">{/* <ImageUploader /> */}</div>
+        <div className="p-2">
+          <ImageUploader />
+        </div>
 
         <div className="p-2">{/* <PasswordForm /> */}</div>
 
